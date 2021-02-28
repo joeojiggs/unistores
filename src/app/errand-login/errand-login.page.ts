a2a8760435e8368a0270a67218204c255bd52f4a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 19:07:43 +0100

    Gitacora saved 1 changed files

--- src/app/errand-login/errand-login.page.ts
+++ src/app/errand-login/errand-login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { MenuController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+import { SignupModalPage } from '../signup-modal/signup-modal.page';
+import { LoginModalPage } from '../login-modal/login-modal.page';
+const { Network, Toast } = Plugins;
 
 @Component({
   selector: 'app-errand-login',
