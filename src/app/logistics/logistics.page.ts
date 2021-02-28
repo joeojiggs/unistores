7a014c1b4638b44e892f2a11e85bd46f06059b40
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 02:45:50 +0100

    Gitacora saved 1 changed files

--- src/app/logistics/logistics.page.ts
+++ src/app/logistics/logistics.page.ts
@@ -10,6 +10,22 @@ export class LogisticsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let ref = this;
+    Network.addListener("networkStatusChange",function 
+    (val)
+    {
+      if (val.connected){
+        ref.showToast("Internet Connected");
+
+      }
+      else{
+        ref.showToast("Internet Disconnected");
+        window.location.assign("/home");
+      }
+    });
+  }
+  showToast(msg){
+    Toast.show({text:msg,duration: 'long'})
   }
 
 }
