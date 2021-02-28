c15ee92d8f40d0561217796804faa43a55a86db9
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 16:59:57 +0100

    Gitacora saved 2 changed files

--- src/app/login-modal/login-modal.page.ts
+++ src/app/login-modal/login-modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.page.html',
