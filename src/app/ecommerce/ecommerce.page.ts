a10caee2177e030c4b4c99580dd3ec453e84b287
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 02:46:38 +0100

    Gitacora saved 1 changed files

--- src/app/ecommerce/ecommerce.page.ts
+++ src/app/ecommerce/ecommerce.page.ts
@@ -12,6 +12,22 @@ export class EcommercePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let ref = this;
+    Network.addListener("networkStatusChange",function 
+    (val)
+    {
+      if (val.connected){
+        ref.showToast("Internet Connected");
+
+      }
+      else{
+        ref.showToast("Internet Disconnected");
+        window.location.assign("/home");
+      }
+    });
+  }
+  showToast(msg){
+    Toast.show({text:msg,duration: 'long'})
   }
 
 }
