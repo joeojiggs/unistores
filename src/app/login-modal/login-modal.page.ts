ace3e6283547d91d787691e898160d5cc8892af3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 16:59:42 +0100

    Gitacora saved 1 changed files

---
+++ src/app/login-modal/login-modal.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.page.html',
+  styleUrls: ['./login-modal.page.scss'],
+})
+export class LoginModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
+  }
+
+}
