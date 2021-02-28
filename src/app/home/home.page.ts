232cd21c9f6e42cade8654ae40b9ac0c38d106e3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 13:12:25 +0100

    Gitacora saved 1 changed files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-
 const { Browser, Network, Toast } = Plugins;
 
 
