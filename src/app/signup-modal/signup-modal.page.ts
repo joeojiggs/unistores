ea32dcffdfe9091d3dc0fd3b7301209eae9364e3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 18:09:42 +0100

    Gitacora saved 1 changed files

--- src/app/signup-modal/signup-modal.page.ts
+++ src/app/signup-modal/signup-modal.page.ts
@@ -28,7 +28,6 @@ export class SignupModalPage implements OnInit {
   }
 
   async logisticsSignup(){
-    this.showToast("No Internet Connectivity");
     let status = await Network.getStatus();
 
     if (status.connected === true ){
