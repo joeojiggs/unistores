277d6fcc2c742b3cea6738d8799ab67701b6cd93
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 14:59:41 +0100

    Gitacora saved 1 changed files

---
+++ src/app/info-center/info-center.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-center',
+  templateUrl: './info-center.page.html',
+  styleUrls: ['./info-center.page.scss'],
+})
+export class InfoCenterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let ref = this;
+    Network.addListener("networkStatusChange",function 
+    (val)
+    {
+      if (val.connected){
+        ref.showToast("Internet Connected");
+
+      }
+      else{
+        ref.showToast("Internet Disconnected");
+        window.location.assign("/home");
+      }
+    });
+  }
+  showToast(msg){
+    Toast.show({text:msg,duration: 'long'})
+  }
+
+}
