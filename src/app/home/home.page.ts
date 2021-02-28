626dd32b75a285bbeacf553cb6a5579ee44455e2
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 16:14:49 +0100

    Gitacora saved 2 changed files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 const { Browser, Network, Toast } = Plugins;
 
