c0118f5e2351b5f11f160c825e6690c0a724966e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 17:38:03 +0100

    Gitacora saved 1 changed files

--- src/app/ecommerce/ecommerce.page.ts
+++ src/app/ecommerce/ecommerce.page.ts
@@ -34,4 +34,75 @@ export class EcommercePage implements OnInit {
     Toast.show({text:msg,duration: 'long'})
   }
 
+  async logistics(){
+    let status = await Network.getStatus();
+
+    if (status.connected === true ){
+      window.location.assign("/logistics");
+    }else{
+      this.showToast("No Internet Connectivity");
+    }
+    
+  }
+
+  async errand(){
+    let status = await Network.getStatus();
+
+    if (status.connected === true ){
+      window.location.assign("/errand");
+    }else{
+      this.showToast("No Internet Connectivity");
+    }
+  }
+
+  async ecommerce(){
+    let status = await Network.getStatus();
+
+    if (status.connected === true ){
+      window.location.assign("/ecommerce");
+    }else{
+      this.showToast("No Internet Connectivity");
+    }
+  }
+
+  async info(){
+    let status = await Network.getStatus();
+
+    if (status.connected === true ){
+      window.location.assign("/info-center");
+    }else{
+      this.showToast("No Internet Connectivity");
+    }
+  }
+
+
+  openMenu() {
+    this.menu.enable(true, 'menu');
+    this.menu.open('menu');
+  }
+
+  closeMenu() {
+    this.menu.close('menu');
+  }
+
+  goHome() {
+    window.location.assign("/home");
+  }
+
+  async signupModal() {
+    const modal = await this.modalController.create({
+      component: SignupModalPage,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
+
+  async loginModal() {
+    const modal = await this.modalController.create({
+      component: LoginModalPage,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
+
 }
