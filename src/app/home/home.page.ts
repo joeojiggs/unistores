84b01369658cae5bcd34849e76375c5f135bdc13
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 16:26:06 +0100

    Gitacora saved 2 changed files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -89,7 +89,7 @@ export class HomePage  implements OnInit{
 
   async presentModal() {
     const modal = await this.modalController.create({
-      component: SignupModalPage
+      component: SignPage,
       cssClass: 'my-custom-class'
     });
     return await modal.present();
