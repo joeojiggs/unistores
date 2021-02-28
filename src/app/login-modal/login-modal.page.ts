43fbb1b2471a18bd49e46921633257058e662ad7
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2021-02-28 18:54:41 +0100

    Gitacora saved 1 changed files

--- src/app/login-modal/login-modal.page.ts
+++ src/app/login-modal/login-modal.page.ts
@@ -30,7 +30,7 @@ export class LoginModalPage implements OnInit {
     let status = await Network.getStatus();
 
     if (status.connected === true ){
-      window.location.assign("/logistics-loginup");
+      window.location.assign("/logistics-login");
     }else{
       this.showToast("No Internet Connectivity");
     }
